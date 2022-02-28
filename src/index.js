f2fd9c0e64d43b29c20f279b09c09f83822e178f
Mehul <mehulrastogi2002@gmail.com>
2022-02-28 17:53:48 -0500

    Merged changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Page1 from './Page1'
 import Page2 from './Page2'
 import Page3 from './Page3'
+import Page35 from './Page35'
 import Page4 from './Page4'
 import Page5 from './Page5'
 import Page6 from './Page6'
@@ -29,6 +30,8 @@ ReactDOM.render(
     <Page2 />
     <Divider />
     <Page3 />
+    <Divider />
+    <Page35 />
     <Divider />
     <Page4 />
     <Divider />
