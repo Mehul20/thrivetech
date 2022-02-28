93936d25b9913efd27f56fd9b596831ede676692
andyng2002 <andyng2002@gmail.com>
2022-02-28 01:57:09 -0500

    Added page4-6

---
+++ src/Page5.js
@@ -0,0 +1,38 @@
+import image from './front.png'
+import './App.css';
+import Button from '@material-ui/core/Button';
+import Page2 from "./Page2";
+  
+
+function Page5() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={image} className="App-logo" alt="logo" /> */}
+        <p className="Textstyle">
+          HELLO
+        </p>
+        <Button color="primary" variant="contained">
+        CHECK IN
+      </Button>
+      <Button color="primary" variant="contained">
+        EVENT CALENDAR
+      </Button>
+      <Button color="primary" variant="contained">
+        RESOURCES
+      </Button>
+      </header>
+     
+      
+
+      
+      
+    </div>
+
+
+
+
+  );
+}
+
+export default Page5;
