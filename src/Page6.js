c3cf466f25f44728bedeacbbf18e3c652fd275f3
Mehul <mehulrastogi2002@gmail.com>
2022-03-09 14:09:35 -0500

    checkin pages designed

--- src/Page6.js
+++ src/Page6.js
@@ -1,8 +1,15 @@
 import './App.css';
 // import AlarmOnIcon from '@mui/icons-material/AlarmOn';
 import Button from '@material-ui/core/Button';
+import {useState} from "react";
+
 
 function Page6() {
+
+  const [isChecked, setIsChecked] = useState(false);
+  const handleOnChange = () => {
+    setIsChecked(!isChecked);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +17,16 @@ function Page6() {
         <p className="Textstyle">
          What are your goals for the day?
          <br /> 
-         <input type="checkbox" id="meditate" name="meditate" value="meditate"></input>
+         <input 
+         type="checkbox" 
+         id="meditate" 
+         name="meditate" 
+         value="meditate"
+         checked = {isChecked}
+         onChange = {handleOnChange}
+         >
+           
+          </input>
          <label for="meditate"> Meditate</label><br></br>
          <input type="checkbox" id="homework" name="homework" value="homework"></input>
          <label for="homework"> Homework</label><br></br>
@@ -23,9 +39,15 @@ function Page6() {
          <input type="checkbox" id="exercise" name="exercise" value="exercise"></input>
          <label for="exercise"> Exercise</label><br></br>
         </p>
-        <Button color="primary" variant="contained">
-        NEXT
-      </Button>
+
+     
+<div>
+      {isChecked ? (
+       <Button color="primary" variant="contained" id="goals" href='#meditation'> NEXT</Button>
+      ) : (
+        <Button color="primary" variant="contained" id="goals"> NEXT </Button>
+      )}
+</div>
       </header>
       
     </div>
