8e303c40abf368cc316d84b0f3d08bbebdd563c8
Mehul <mehulrastogi2002@gmail.com>
2022-03-09 14:39:49 -0500

    Navigation for Page 6 done

--- src/Page6.js
+++ src/Page6.js
@@ -1,15 +1,19 @@
 import './App.css';
-// import AlarmOnIcon from '@mui/icons-material/AlarmOn';
 import Button from '@material-ui/core/Button';
 import {useState} from "react";
 
 
 function Page6() {
 
-  const [isChecked, setIsChecked] = useState(false);
-  const handleOnChange = () => {
-    setIsChecked(!isChecked);
+  const [isChecked, setIsChecked] = useState(new Array(6).fill(false))
+  const handleOnChange = (position) => {
+    const updatedCheckState = isChecked.map((item, index) => 
+        index === position ? !item : item
+  );
+  setIsChecked(updatedCheckState);
   }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,35 +22,92 @@ function Page6() {
          What are your goals for the day?
          <br /> 
          <input 
-         type="checkbox" 
+         type="radio" 
          id="meditate" 
          name="meditate" 
          value="meditate"
-         checked = {isChecked}
-         onChange = {handleOnChange}
+         checked = {isChecked[0]}
+         onChange = {() => handleOnChange(0)}
          >
            
           </input>
          <label for="meditate"> Meditate</label><br></br>
-         <input type="checkbox" id="homework" name="homework" value="homework"></input>
+
+
+
+         <input 
+         type="radio" 
+         id="homework" 
+         name="homework" 
+         value="homework"
+         checked = {isChecked[1]}
+         onChange = {() => handleOnChange(1)}
+         >
+           
+           
+           </input>
          <label for="homework"> Homework</label><br></br>
-         <input type="checkbox" id="relaxation" name="relaxation" value="relaxation"></input>
+
+
+         <input type="radio" 
+         id="relaxation" 
+         name="relaxation" 
+         value="relaxation"
+         checked = {isChecked[2]}
+         onChange = {() => handleOnChange(2)}
+         >
+          </input>
          <label for="relaxation"> Relaxation</label><br></br>
-         <input type="checkbox" id="studying" name="studying" value="studying"></input>
+
+
+
+
+         <input type="radio" id="studying" name="studying" value="studying"
+         checked = {isChecked[3]}
+         onChange = {() => handleOnChange(3)}
+         
+         ></input>
          <label for="studying"> Studying</label><br></br>
-         <input type="checkbox" id="hobbies" name="hobbies" value="hobbies"></input>
+
+
+
+
+         <input type="radio" id="hobbies" name="hobbies" value="hobbies"
+         checked = {isChecked[4]}
+         onChange = {() => handleOnChange(4)}
+         ></input>
          <label for="hobbies"> Hobbies</label><br></br>
-         <input type="checkbox" id="exercise" name="exercise" value="exercise"></input>
+         <input type="radio" id="exercise" name="exercise" value="exercise"
+         checked = {isChecked[5]}
+         onChange = {() => handleOnChange(5)}
+         
+         ></input>
          <label for="exercise"> Exercise</label><br></br>
         </p>
 
      
 <div>
-      {isChecked ? (
+      
+      {
+      
+      isChecked[0] ? (
        <Button color="primary" variant="contained" id="goals" href='#meditation'> NEXT</Button>
-      ) : (
+      ) : isChecked[1] ? (
+        <Button color="primary" variant="contained" id="goals" href='#meditation'> NEXT</Button>
+      ) : isChecked[2] ? (
+        <Button color="primary" variant="contained" id="goals" href='#meditation'> NEXT</Button>
+      ) : isChecked[3] ? (
+        <Button color="primary" variant="contained" id="goals" href='#meditation'> NEXT</Button>
+      ) : isChecked[4] ? (
+        <Button color="primary" variant="contained" id="goals" href='#meditation'> NEXT</Button>
+      ): isChecked[5] ? (
+        <Button color="primary" variant="contained" id="goals" href='#meditation'> NEXT</Button>
+      ) :
+      (
         <Button color="primary" variant="contained" id="goals"> NEXT </Button>
-      )}
+      )
+      
+      }
 </div>
       </header>
       
