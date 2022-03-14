c519b5928f61b4c056ee1295e857eda1082c7f7a
andyng2002 <andyng2002@gmail.com>
2022-03-14 19:28:56 -0400

    added information to resources

--- src/resources.js
+++ src/resources.js
@@ -13,13 +13,13 @@ function resources() {
             Mental Health
         </h2>
         <p>
-            Thrive@GT
+            Thrive@GT: Wellness portal of mental health and well-being resources provided in partnership through the University System of Georgia's Mental Health Initiative.
         </p>
         <p>
-            VOICE
+            VOICE: VOICE provides support to victim-survivors of sexual violence and those supporting survivors as well as educates the campus community about healthy relationships and sexual violence.
         </p>
         <p>
-            GT Counseling
+            GT Counseling: The Counseling Center offers a full range of counseling and psychological services to help facilitate lifelong personal development, promote mental health, and prevent or reduce stress
         </p>
         <h2>
             Meditation
