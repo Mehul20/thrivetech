008cbcd4b6d87fded20c12b34df936c841a2a0fa
andyng2002 <andyng2002@gmail.com>
2022-04-06 15:29:23 -0400

    added to resources page

--- src/resources.js
+++ src/resources.js
@@ -1,34 +1,60 @@
+import './index.css';
 import './App.css';
-import SelfImprovementIcon from '@mui/icons-material/SelfImprovement';
+import Book from '@mui/icons-material/Book';
 
 function resources() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
+        <Book style={{ fontSize: 150, color:'black' }}/>
+        <h1 className="heading">
             RESOURCES
         </h1>
-        <h2>
+        <h2 className="heading"> 
             Mental Health
         </h2>
-        <p>
+        <p className="Textstyle">
             Thrive@GT: Wellness portal of mental health and well-being resources provided in partnership through the University System of Georgia's Mental Health Initiative.
         </p>
-        <p>
+        <p className="Textstyle">
             VOICE: VOICE provides support to victim-survivors of sexual violence and those supporting survivors as well as educates the campus community about healthy relationships and sexual violence.
         </p>
-        <p>
+        <p className="Textstyle">
             GT Counseling: The Counseling Center offers a full range of counseling and psychological services to help facilitate lifelong personal development, promote mental health, and prevent or reduce stress
         </p>
-        <h2>
+        <h2 className="heading">
             Meditation
         </h2>
-        <h2>
+        <p className="Textstyle">
+           Kadampa Meditation Center Georgia: a Buddhist meditation center dedicated to the attainment of world peace by offering everyone the opportunity to develop lasting mental peace through meditation and study programs.
+        </p>
+        <p className="Textstyle">
+            Atlanta Soto Zen Center: ASZC supports its members by providing a Sangha, or group to sit with. Zazen, or sitting meditation, is available mornings and evenings every day of the week. Experienced teachers are available to respond to any questions that arise.
+        </p>
+        <p className="Textstyle">
+            Websites: Mindful.org, Headspace.com, Calm.com
+        </p>
+        <h2 className="heading">
             Exercise
         </h2>
-        <h2>
+        <p className="Textstyle">
+           Campus Recreation Center: From a state-of-the-art fitness center, world-class aquatics facility,  the CRC offers fitness programs, competitive sports, aquatic programs, outdoor recreation, and more.
+        </p>
+        <h2 className="heading">
             Academic Assistance
         </h2>
+        <p className="Textstyle">
+          1-to-1 Tutoring: 1-to-1 Tutoring is a free, appointment-based tutoring program offered to all Georgia Tech undergraduate students.
+        </p>
+        <p className="Textstyle">
+          PLUS: PLUS sessions are group study sessions that facilitate learning in a relaxed, collaborative environment. 
+        </p>
+        <p className="Textstyle">
+          Drop-In Tutoring: Drop-In Tutoring is offered for immediate help from course TAs and our tutors – no appointment necessary! 
+        </p>
+        <p className="Textstyle">
+          Learning Assitant Program: Learning Assistants facilitate student learning and small group discussion inside the classroom.
+        </p>
       </header>
       
     </div>
